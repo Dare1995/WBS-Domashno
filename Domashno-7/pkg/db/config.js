dce46c8e390c43e81f7cec0645b0358e493e9734
Dare1995 <darko_kiprijanovski@hotmail.com>
2024-09-10 19:33:05 +0200

    Domashno-7

---
+++ Domashno-7/pkg/db/config.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const uri =
+  "";
+
+async function connect() {
+  try {
+    await mongoose.connect(uri);
+    console.log("MongoDB connected!");
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// connect();
+module.exports = connect;
